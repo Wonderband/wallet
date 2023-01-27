314ab086eac19fc2a63bbe5e26291131b136d73d
Maksym Stelmakh <stelmakh1994@gmail.com>
2023-01-27 13:56:14 +0200

    add register form, authAPI

--- src/index.js
+++ src/index.js
@@ -3,11 +3,18 @@ import ReactDOM from 'react-dom/client';
 import { App } from 'components/App';
 import './index.css';
 import { BrowserRouter } from 'react-router-dom';
+import { persistor, store } from 'redux/store';
+import { Provider } from 'react-redux';
+import { PersistGate } from 'redux-persist/integration/react';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <BrowserRouter basename="/wallet">
-      <App />
-      </BrowserRouter>
+      <Provider store={store}>
+        <PersistGate persistor={persistor}>
+          <App />
+        </PersistGate>
+      </Provider>
+    </BrowserRouter>
   </React.StrictMode>
 );
---
+++ src/services/authAPI.js
@@ -0,0 +1,40 @@
+import axios from 'axios';
+
+const BASE_URL = 'https://wallet.goit.ua';
+
+const auth = axios.create({
+  baseURL: BASE_URL,
+});
+
+export const setAuthToken = token => {
+  auth.defaults.headers.common.Authorization = `Bearer ${token}`;
+};
+
+const clearAuthToken = () => {
+  auth.defaults.headers.common.Authorization = '';
+};
+
+export const authAPI = {
+  async registerUser(formData) {
+    const response = await auth.post('/api/auth/sign-up', formData);
+    setAuthToken(response.data.token);
+    return await response.data;
+  },
+
+  async loginUser(formData) {
+    const response = await auth.post('/api/auth/sign-in', formData);
+    setAuthToken(response.data.token);
+    return response.data;
+  },
+
+  async logoutUser() {
+    const response = await auth.delete('/api/auth/sign-out');
+    clearAuthToken();
+    return response.data;
+  },
+
+  async refreshUser() {
+    const response = await axios('/api/users/current');
+    return response.data;
+  },
+};
