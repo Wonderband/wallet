296533f04a7a3bd05d5aee24c0f0461061d9173e
Veronica <vveronikanika@gmail.com>
2023-01-28 18:29:44 +0200

    style: selects, add custom arrow

--- src/components/Table/Table.jsx
+++ src/components/Table/Table.jsx
@@ -1,15 +1,25 @@
 import MONTHS from '../../constants/months';
 import { YEARS } from '../../constants/lastFiveYears';
+// import { useEffect } from 'react';
+
+import css from './Table.module.css';
 
 export const Table = () => {
   const handleChange = e => {
     console.log(e.target.value);
   };
 
+  // Треба відправити запит для відображення актуальної інформації в табличці, коли користувач тільки заходить на сторінку.
+  // В якому компоненті це робити? На рівень вище?
+
+  // const statisticTableForCurrentMonth =
+  //     console.log(YEARS[0]);
+  //     console.log(MONTHS[0]);
+
   return (
-    <div>
-      <label>
-        <select onChange={handleChange}>
+    <div className={css.inputs}>
+      <label className={css.selectWrapper}>
+        <select className={css.select} onChange={handleChange}>
           {MONTHS.map(option => (
             <option key={option} value={option}>
               {option}
@@ -17,8 +27,8 @@ export const Table = () => {
           ))}
         </select>
       </label>
-      <label>
-        <select onChange={handleChange}>
+      <label className={css.selectWrapper}>
+        <select className={css.select} onChange={handleChange}>
           {YEARS.map(option => (
             <option key={option} value={option}>
               {option}
