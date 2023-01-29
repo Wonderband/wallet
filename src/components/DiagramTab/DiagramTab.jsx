d964f6e8ac367fb226e26bb9a9c3494052adf413
Veronica <vveronikanika@gmail.com>
2023-01-30 00:35:25 +0200

    feat: add second part of Table component,

--- src/components/DiagramTab/DiagramTab.jsx
+++ src/components/DiagramTab/DiagramTab.jsx
@@ -1,11 +1,21 @@
+// import { useDispatch } from 'react-redux';
 import { Chart } from 'components/Chart/Chart';
 import { Table } from 'components/Table/Table';
 
 export const DiagramTab = () => {
+  // const dispatch = useDispatch();
+
+  // useEffect(() => {
+  //   dispatch(refreshUser());
+  // }, [dispatch]); ????????
+
   return (
     <>
       <div>I'm DIAGRAM TAB</div>
       <Chart />
+      {/* balance={balance}
+          data={stats?.data ? stats.data : []}
+          colors={colors} */}
       <Table />
     </>
   );
