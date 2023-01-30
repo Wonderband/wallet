8904ac5451f9f9f497396e3913a9a558500a72cc
Alex Koniushenko <alex.koniushenko@gmail.com>
2023-01-30 22:15:40 +0200

    Update DashboardPage.jsx

--- src/pages/DashBoardPage/DashboardPage.jsx
+++ src/pages/DashBoardPage/DashboardPage.jsx
@@ -1,9 +1,9 @@
 import { Balance } from 'components/Balance/Balance';
 import { Currency } from 'components/Currency/Currency';
 import { Navigation } from 'components/Navigation/Navigation';
-// import { useSelector } from 'react-redux';
+import { useSelector } from 'react-redux';
 import { Header } from '../../components/Header/Header';
-// import { selectAuthToken } from '../../redux/selectors';
+import { selectAuthToken } from '../../redux/selectors';
 import { useEffect } from 'react';
 import { useDispatch } from 'react-redux';
 import { Outlet } from 'react-router-dom';
@@ -11,11 +11,6 @@ import { getTransactions } from 'redux/finance/financeOperations';
 
 // import css from './DashboardPage.module.css';
 export const DashboardPage = () => {
-  const dispatch = useDispatch();
-  useEffect(() => {
-    dispatch(getTransactions());
-  }, [dispatch]);
-
   return (
     <>
       <Header />
