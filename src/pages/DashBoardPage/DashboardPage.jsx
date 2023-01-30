1e1079fb8378c8c4e446f3fe0810292ac622ce55
YZarytskyi <yurazar222@gmail.com>
2023-01-30 19:45:27 +0200

    change routes strtucture

--- src/pages/DashBoardPage/DashboardPage.jsx
+++ src/pages/DashBoardPage/DashboardPage.jsx
@@ -1,11 +1,16 @@
 import { Balance } from 'components/Balance/Balance';
 import { Currency } from 'components/Currency/Currency';
 import { Navigation } from 'components/Navigation/Navigation';
+import { useSelector } from 'react-redux';
 import { Outlet } from 'react-router-dom';
+import { Header } from '../../components/Header/Header';
+import { selectAuthToken } from '../../redux/selectors';
 // import css from './DashboardPage.module.css';
 export const DashboardPage = () => {
+  const isAuth = useSelector(selectAuthToken);
   return (
     <>
+      {isAuth && <Header />}
       <div>I'm DASHBOARD page!</div>
       <Navigation />
       <Balance />
