dcc589abfd13330c1eb588b87dd36a34060aad1f
Alex Koniushenko <alex.koniushenko@gmail.com>
2023-01-30 13:54:49 +0200

    Add transaction working

--- src/redux/finance/financeOperations.js
+++ src/redux/finance/financeOperations.js
@@ -3,7 +3,18 @@ import { auth } from 'services/authAPI';
 
 async function getAllCategories(_, thunkAPI) {
   try {
-  const res =  await auth.get('/api/transaction-categories');
+    const res = await auth.get('/api/transaction-categories');   
+    return res.data;
+  } catch (error) {
+    thunkAPI.rejectWithValue(error.message);
+  }
+}
+
+async function createNewTransaction(transData, thunkAPI) {
+  try {
+    // console.log(transData);
+    const res = await auth.post('/api/transactions', transData);
+    console.log(res)
     return res.data;
   } catch (error) {
     thunkAPI.rejectWithValue(error.message);
@@ -13,4 +24,9 @@ async function getAllCategories(_, thunkAPI) {
 export const getCategories = createAsyncThunk(
   'finance/categories',
   getAllCategories
+);
+
+export const createTransaction = createAsyncThunk(
+  'finance/createTransaction',
+  createNewTransaction
 );
