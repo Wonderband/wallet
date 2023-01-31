4920858f4f62e13160cab3ee735c0aae45430fa6
Olha Prymak <113449585+peckeby@users.noreply.github.com>
2023-01-31 19:05:35 +0200

    Create diagram with colors and data logics

--- src/components/DiagramTab/DiagramTab.jsx
+++ src/components/DiagramTab/DiagramTab.jsx
@@ -1,11 +1,11 @@
-import { Chart } from 'components/Chart/Chart';
+import { ChartView } from 'components/ChartView/ChartView';
 import { Table } from 'components/Table/Table';
 
 export const DiagramTab = () => {
   return (
     <>
       <div>I'm DIAGRAM TAB</div>
-      <Chart />
+      <ChartView />
       <Table />
     </>
   );
