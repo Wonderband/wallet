deac8b17277182ee3caaf54ea8bf977b1f17b5ea
Maksym Stelmakh <stelmakh1994@gmail.com>
2023-02-01 15:45:37 +0200

    add 404page, remove unnessesary images

--- src/assets/images/LoginPage/webp/index.js
+++ src/assets/images/LoginPage/webp/index.js
@@ -1,6 +1,4 @@
-import loginWebP270w from './login270w.webp';
-import loginWebP450w from './login450w.webp';
 import loginWebP540w from './login540w.webp';
 import loginWebP900w from './login900w.webp';
 
-export { loginWebP270w, loginWebP450w, loginWebP540w, loginWebP900w };
+export { loginWebP540w, loginWebP900w };
