4185207ad08e803b387b4f8fb29e0e6cf2648d6e
Oleksandr Kryvoniuk <sasha12887@gmail.com>
2023-02-01 20:33:41 +0200

    create portal

--- src/components/ModalLogOut/ModalLogOut.jsx
+++ src/components/ModalLogOut/ModalLogOut.jsx
@@ -1,4 +1,5 @@
 import { useEffect } from 'react';
+import { createPortal } from 'react-dom';
 import { useDispatch } from 'react-redux';
 import { closeLogOutModal } from 'redux/global/globalSlice';
 import { logOut } from 'redux/session/sessionOperations';
@@ -27,14 +28,14 @@ export const ModalLogOut = () => {
       window.removeEventListener('keydown', closeModal);
     };
   }, [dispatch]);
-
-  return (
-    <div className={css.modalBackdrop} onClick={clickOnBackdropHandler}>
-      <section className={css.modalSection}>
-        <h2 className={css.modalTitle}>Are you sure?</h2>
-          <button type='button' className={css.modalYesBtn} onClick={onLogOut}>Yes</button>
-          <button type='button' className={css.modalCancelBtn} onClick={() => {dispatch(closeLogOutModal())}}>Cancel</button>
-      </section>
-    </div>
+  
+  return createPortal(
+      <div className={css.modalBackdrop} onClick={clickOnBackdropHandler}>
+        <section className={css.modalSection}>
+          <h2 className={css.modalTitle}>Are you sure?</h2>
+            <button type='button' className={css.modalYesBtn} onClick={onLogOut}>Yes</button>
+            <button type='button' className={css.modalCancelBtn} onClick={() => {dispatch(closeLogOutModal())}}>Cancel</button>
+        </section>
+      </div>, document.querySelector('#modal')
   );
 };
