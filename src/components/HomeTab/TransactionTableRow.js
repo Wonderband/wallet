645b48da0430e6c373c912692e0f1682e553785f
gtgambit <gtgambit@gmail.com>
2023-02-01 23:57:11 +0200

    sort + balanceFix

--- src/components/HomeTab/TransactionTableRow.js
+++ src/components/HomeTab/TransactionTableRow.js
@@ -5,28 +5,40 @@ const TransactionTableRow = ({ transaction }) => {
   const { transactionDate, type, categoryId, comment, amount, balanceAfter } =
     transaction;
   return (
-    <tr className={css.table__tr}>
-      <td className={css.tdDate}>{`${new Date(transactionDate)
-        .getDate()
-        .toString()
-        .padStart(2, '0')}.${(new Date(transactionDate).getMonth() + 1)
-        .toString()
-        .padStart(2, '0')}.${new Date(transactionDate)
-        .getFullYear()
-        .toString()
-        .slice(2)}`}</td>
-      <td className={css.tdType}>{type === 'INCOME' ? '+' : '-'}</td>
-      <td className={css.tdCategory}>
-        {<CategoryName categoryId={categoryId} />}
-      </td>
-      <td className={css.tdComment}>{comment}</td>
-      <td className={amount > 0 ? css.income : css.expense}>
-        {amount < 0 ? amount * -1 : amount}
-      </td>
-      <td className={css.tdBalance}>
-        {balanceAfter ? Math.abs(balanceAfter.toFixed(2)) : 0}
-      </td>
-    </tr>
+    <>
+      <tr className={css.table__tr}>
+        <td className={css.tdDate}>{`${new Date(transactionDate)
+          .getDate()
+          .toString()
+          .padStart(2, '0')}.${(new Date(transactionDate).getMonth() + 1)
+          .toString()
+          .padStart(2, '0')}.${new Date(transactionDate)
+          .getFullYear()
+          .toString()
+          .slice(2)}`}</td>
+        <td className={css.tdType}>{type === 'INCOME' ? '+' : '-'}</td>
+        <td className={css.tdCategory}>
+          {<CategoryName categoryId={categoryId} />}
+        </td>
+        <td className={css.tdComment}>{comment}</td>
+        <td className={amount > 0 ? css.income : css.expense}>
+          {amount < 0 ? amount * -1 : amount}
+        </td>
+        <td className={css.tdBalance}>
+          {balanceAfter ? Math.abs(balanceAfter.toFixed(2)) : 0}
+          <button onClick={() => setShowModal(true)}>edit</button>{' '}
+          <button onClick={() => dispatch(deleteTransaction(transaction.id))}>
+            delete
+          </button>
+        </td>
+      </tr>
+      {showModal && (
+        <ModalAddTransaction
+          transaction={transaction}
+          closeModal={closeModal}
+        />
+      )}
+    </>
   );
 };
 
